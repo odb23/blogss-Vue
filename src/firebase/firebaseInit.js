29d80539a82ff88dc379530c4912e5efe523d449
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-12-28 17:02:07 +0100

    project setup

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCF-nJF5vxG9SHolmV6wZn3_N560kMKuWY",
+  authDomain: "odblogss.firebaseapp.com",
+  projectId: "odblogss",
+  storageBucket: "odblogss.appspot.com",
+  messagingSenderId: "700333114039",
+  appId: "1:700333114039:web:6ff81dcab1b7795eccc332"
+};
+
+const app = initializeApp(firebaseConfig);
+const timestamp = serverTimestamp
+const firestore = getFirestore(app)
+
+export {timestamp}
+export default firestore
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {Vue2Editor} from "vue2-editor"
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vue2Editor)
 
 app.mount('#app')
