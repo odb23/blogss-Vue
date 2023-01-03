1b5b19cd25d52e7d3306312220d521a780852636
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2023-01-03 15:04:50 +0100

    added login and register views

---
+++ src/views/LoginView.vue
@@ -0,0 +1,157 @@
+<template >
+    <div class="form-wrap">
+        <form action="" class="login">
+            <p class="login-register">
+                Don't have an account?
+                <RouterLink class="router-link" :to="{ name: 'Register' }">Register</RouterLink>
+            </p>
+            <h2>Login to ODBlogss</h2>
+            <div class="inputs">
+                <div class="input">
+                    <input type="text" placeholder="Email" v-model="email">
+                    <img :src="Email" alt="" class="icon">
+                </div>
+                <div class="input">
+                    <input type="pasword" placeholder="Password" v-model="password">
+                    <img :src="Password" alt="" class="icon">
+                </div>
+            </div>
+
+            <RouterLink class="forgot-password" :to="{ name: 'ForgotPassword' }">
+                Forgot your password?
+            </RouterLink>
+
+            <button>Sign In</button>
+            <div class="angle"></div>
+        </form>
+        <div class="background">
+
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref } from "vue"
+import Email from "../assets/Icons/envelope-regular.svg"
+import Password from "../assets/Icons/lock-alt-solid.svg"
+
+let email = ref(null)
+let password = ref(null)
+
+</script>
+<style lang="scss" >
+.form-wrap {
+    overflow: hidden;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-self: center;
+    margin: 0 auto;
+    width: 90%;
+
+    @media (min-width: 900px) {
+        width: 100%;
+    }
+
+    .login-register {
+        margin-bottom: 32px;
+
+        .router-link {
+            color: black
+        }
+    }
+
+    form {
+        padding: 0 10px;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+
+        @media (min-width: 900px) {
+            padding: 0 50px;
+        }
+
+        h2 {
+            text-align: center;
+            font-size: 32px;
+            color: #303030;
+            margin-bottom: 40px;
+
+            @media (min-width: 900px) {
+                font-size: 40px;
+            }
+        }
+
+        .inputs {
+            width: 100%;
+            max-width: 350px;
+
+            .input {
+                position: relative;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-bottom: 8px;
+
+                input {
+                    width: 100%;
+                    border: none;
+                    background-color: #f2f7f6;
+                    padding: 4px 4px 4px 30px;
+                    height: 50px;
+
+                    &:focus {
+                        outline: none;
+                    }
+                }
+                .icon {
+                    width: 12px;
+                    position: absolute;
+                    left: 6px;
+                }
+            }
+        }
+
+        .forgot-password {
+            text-decoration: none;
+            color: black;
+            cursor: pointer;
+            font-size: 14px;
+            margin: 16px 0 32px;
+            border-bottom: 1px solid transparent;
+            transition: .5s ease all;
+
+            &:hover {
+                border-color: #303030;
+            }
+        }
+
+        .angle{
+            display: none;
+            position: absolute;
+            background-color: #fff;
+            transform: rotateZ(3deg);
+            width: 60px;
+            right: -30px;
+            height: 101%;
+            @media (min-width: 900px) {
+                display: initial;
+            }
+        }
+    }
+
+    .background {
+        display: none;
+        flex: 2;
+        background-size: cover;
+        background-image: url("../assets/background.png");
+        width: 100%;
+        height: 100%;
+        @media (min-width: 900px) {
+            display: initial
+        }
+    }
+}
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,59 @@
+<template >
+    <div class="form-wrap">
+        <form action="" class="register">
+            <p class="login-register">
+               Already have an account?
+                <RouterLink class="router-link" :to="{ name: 'Login' }">Log In</RouterLink>
+            </p>
+            <h2>Create your ODBlogss account</h2>
+            <div class="inputs">
+                <div class="input">
+                    <input type="text" placeholder="First Name" v-model="firstName">
+                    <img :src="User" alt="" class="icon">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Last Name" v-model="lastName">
+                    <img :src="User" alt="" class="icon">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Username" v-model="username">
+                    <img :src="User" alt="" class="icon">
+                </div>
+                <div class="input">
+                    <input type="text" placeholder="Email" v-model="email">
+                    <img :src="Email" alt="" class="icon">
+                </div>
+                <div class="input">
+                    <input type="pasword" placeholder="Password" v-model="password">
+                    <img :src="Password" alt="" class="icon">
+                </div>
+            </div>
+
+            <button>Sign Up</button>
+            <div class="angle"></div>
+        </form>
+        <div class="background">
+
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref } from "vue"
+import Email from "../assets/Icons/envelope-regular.svg"
+import Password from "../assets/Icons/lock-alt-solid.svg"
+import User from "../assets/Icons/user-alt-light.svg"
+
+let email = ref(null)
+let password = ref(null)
+let firstName= ref(null)
+let lastName = ref(null)
+let username = ref(null)
+
+</script>
+<style lang="scss" scoped>
+.register {
+    h2 {
+        max-width: 350px;
+    }
+}
+</style>
